4a14fecdce854ed60cd3186867f4c0d8fe449f13
ryanarj <karshan813@gmail.com>
2018-12-11 15:41:57 -0500

    Added templates

--- sports_blog/settings.py
+++ sports_blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sports_blog_app.apps.SportsBlogAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
