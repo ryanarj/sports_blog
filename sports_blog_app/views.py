4ffbacd1787c2a9d0be989929e46356466dfcf23
ryanarj <karshan813@gmail.com>
2018-12-18 16:39:09 -0500

    Got rid of semantic UI and added bootstrap

--- sports_blog_app/views.py
+++ sports_blog_app/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import ExampleForm
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
 
 def home(request):
-    context = {'form': ExampleForm}
-    return render(request, 'blog/home.html', context)
+    return render(request, 'blog/home.html')
