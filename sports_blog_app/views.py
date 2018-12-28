47376759ddcc91135291c2fdbb1609cf2d7fa3a9
ryanarj <karshan813@gmail.com>
2018-12-28 09:04:11 -0500

    Blog posts can be seen from the models

--- sports_blog_app/views.py
+++ sports_blog_app/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
 
-posts = [
-    {
-    'author' : 'Ryan',
-    'title' : 'Sell the Knicks',
-    'content' : 'THe knicks need to be sold!!',
-    'date_posted' : '12/18/2018'
-    }
-]
-
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
