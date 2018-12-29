972d73a610e489e007ed4fa2f2b3bd19f54c3e1b
ryanarj <karshan813@gmail.com>
2018-12-29 12:41:55 -0500

    POST for user registration

--- sports_blog_users_app/views.py
+++ sports_blog_users_app/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'User has been added')
+    else:
+        form = UserCreationForm()
+
     return render(request, 'users/register.html', {'form': form})
 
 def login(request):
