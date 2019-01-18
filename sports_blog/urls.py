7b7152fb94732fcfc1aaa62cf1c43753b54b7a48
ryanarj <karshan813@gmail.com>
2019-01-17 20:06:21 -0500

    Links for signing in and signing up

--- sports_blog/urls.py
+++ sports_blog/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include 
 from sports_blog_users_app import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('login/', user_views.login, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('sports_blog_app.urls')),
 
 ]
