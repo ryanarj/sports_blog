cef933d5e5aa134eddf0e247e30c1108d72795c0
ryanarj <karshan813@gmail.com>
2019-01-17 19:04:41 -0500

    User can now be registered

--- sports_blog_users_app/views.py
+++ sports_blog_users_app/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid:
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'User has been added')
+            messages.success(request, f'User {username} has been added!')
+            return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
 
