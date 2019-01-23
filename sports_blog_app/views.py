f2490993ba886464e212da6906ebefed79fe395b
ryanarj <karshan813@gmail.com>
2019-01-23 13:37:39 -0500

    Single Post view

--- sports_blog_app/views.py
+++ sports_blog_app/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
-
-def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
 
 def home(request):
     context = {
         'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
