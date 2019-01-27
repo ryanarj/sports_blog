a1b05e99de85c38c7125a44e21162a5dbb81e7b0
ryanarj <karshan813@gmail.com>
2019-01-27 16:12:24 -0500

    Applying Blog type

--- sports_blog_app/urls.py
+++ sports_blog_app/urls.py
@@ -4,8 +4,10 @@ from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('mlb/', PostListView.as_view(), name='blog-mlb'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete')
 ]
+       
--- sports_blog_app/views.py
+++ sports_blog_app/views.py
@@ -13,15 +13,31 @@ def home(request):
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
 
-class PostListView(ListView):
-    model = Post
-    template_name = 'blog/home.html'
-    context_object_name = 'posts'
-    ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+
+class PostListView(ListView):
+    def get_queryset(self):
+        # original qs
+        qs = super().get_queryset() 
+        # filter by a variable captured from url, for example
+        return qs.filter(name__startswith=self.kwargs['blog_type'])
+    def get_slug_field(self):
+        """
+        Get the name of a slug field to be used to look up by slug.
+        """
+        return self.slug_field
+
+    query_blog_name = get_queryset
+    model = Post
+    if query_blog_name == 'MLB':
+        template_name = 'blog/mlb_page.html'
+    else:
+        template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
