0c63c9b37a56b24cf036a1f9c296b3f37f94c622
ryanarj <karshan813@gmail.com>
2019-02-06 12:55:43 -0500

    Add changes

--- sports_blog/settings.py
+++ sports_blog/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'sports_blog_app.apps.SportsBlogAppConfig',
     'sports_blog_chat.apps.SportsBlogChatConfig',
     'crispy_forms',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
